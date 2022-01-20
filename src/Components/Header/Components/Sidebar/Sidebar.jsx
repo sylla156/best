fa50bfda9c17a08572024c393e0432915ad79e7b
sylla ibrhim <s67986601@gmail.com>
2022-01-20 15:21:19 +0000

    build:sidebarFinish

--- src/Components/Header/Components/Sidebar/Sidebar.jsx
+++ src/Components/Header/Components/Sidebar/Sidebar.jsx
@@ -61,9 +61,9 @@ function MouseEffect(event) {
   mouse.x = event.pageX;
   mouse.y = event.pageY;
   let img = document.querySelector(".navImg");
-
-  img.style.top =  mouse.y + 100+ "px";
-  img.style.left = mouse.x - 100+ "px";
+  
+      img.style.top =  mouse.y + 100+ "px";
+      img.style.left = mouse.x - 400+ "px";
 
 
   switch (event.target.innerText) {
