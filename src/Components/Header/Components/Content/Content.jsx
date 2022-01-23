fd5049af46707f1fbb987474007ff6a6e5869dff
sylla ibrhim <s67986601@gmail.com>
2022-01-23 16:14:30 +0000

    build:HeaderContent

---
+++ src/Components/Header/Components/Content/Content.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './content.scss';
+
+export default function Content({}) {
+
+
+    return (
+        <div className="content">
+            <div className="content__title">
+                <h1 className='content__title--title'>developper</h1>
+                <h1 className='content__title--title'>full stack</h1>
+            </div>
+           
+        </div>
+    );
+}
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,20 +3,22 @@ import "./Header.scss";
 import './Components/Loader/Loader';
 import Loader from "./Components/Loader/Loader";
 import Sidebar from './Components/Sidebar/Sidebar';
-import Content from '../../assets/js/Content';
 import ParallaxEffect from './Components/ParallaxEffect/ParallaxEffect.jsx';
 import Reseau from "./Components/Reseau/Reseau";
+import Content from './Components/Content/Content'
+import websiteContent from '../../assets/js/websiteContent'
 
 export default function Header() {
  
-const navTitle = Content().header.navTitle;
-const title = Content().header.title;
+const navTitle = websiteContent().header.navTitle;
+const title = websiteContent().header.title;
   return (
     <header className="header">
-     {/* <Loader/> */}
+     <Loader/>
      <Sidebar title={title} navTitle={navTitle} />
-     {/* <ParallaxEffect/> */}
+     <ParallaxEffect/>
      <Reseau/>
+     <Content/>
     </header>
   );
 }
