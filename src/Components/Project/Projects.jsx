d62429b640a9815ce4002928d8d585a0d114ed47
sylla ibrhim <s67986601@gmail.com>
2022-02-08 08:20:00 +0000

    refactor:project

--- src/Components/Project/Projects.jsx
+++ src/Components/Project/Projects.jsx
@@ -29,20 +29,33 @@ export default function Projects({ onChangeVisibility }) {
 
   return (
     <div className="projects" id="project">
+      <div className="projects__title">
+        <h1>
+          <span>p</span>
+          <span>r</span>
+          <span>o</span>
+          <span>j</span>
+          <span>e</span>
+          <span>c</span>
+          <span>t</span>
+          <span>s</span>
+        </h1>
+      </div>
       <nav className="projects__nav">
         {titles.map((item, index) => {
           return (
-            <div
+            <section
               onMouseEnter={isMobile ? console.log() : hanbleChangeBackGround}
               onMouseMove={isMobile ? hanbleChangeBackGround : console.log()}
+              onMouseLeave={reset}
               onClick={handleProject}
               data-position={index}
               key={index}
             >
-              <p data-position={index}>
+              <p >
                 <span>0.{1 + index}</span> {item}
               </p>
-            </div>
+            </section>
           );
         })}
       </nav>
@@ -52,9 +65,9 @@ export default function Projects({ onChangeVisibility }) {
 
 const hanbleChangeBackGround = function (e) {
   const value = e.target.dataset.position;
-  const nameClass = ["space", "etoile"];
-  const bg = document.querySelector(".projects");
-  const element = document.querySelectorAll(".projects__nav div p");
+  const nameClass = ["space", "commerce"];
+  const bg = e.target;
+  const element = document.querySelectorAll(".projects__nav section");
   for (let index = 0; index < nameClass.length; index++) {
     const item = nameClass[index];
     if (index == value) {
@@ -66,4 +79,8 @@ const hanbleChangeBackGround = function (e) {
       element[index].classList.remove("enable");
     }
   }
+};
+
+const reset = function (e) {
+  e.target.classList.remove("space", "commerce");
 };
